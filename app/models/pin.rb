a1a5aa19ee9060b23bbe436f83093bc9dc1d3966
Marko Manojlovic <marko@thecookie.org>
2015-02-28 15:24:29 -0800

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	validates :description, presence: true
+	validates :image, presence: true
 end
